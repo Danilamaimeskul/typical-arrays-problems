a2978e33ff4fb8b9afed2d05d453356b9b807eff
Danilamaimeskul <59773922+Danilamaimeskul@users.noreply.github.com>
2021-01-30 16:43:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(array.length == 0) return 0;
+    let result = array[0];
+    for(let i in array){
+        if ( result > array[i] ) result = array[i];
+    }
+    return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+      if(array.length == 0) return 0;
+    let result = array[0];
+    for(let i in array){
+        if ( result < array[i] ) result = array[i];
+    }
+    return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(array.length == 0) return 0;
+    
 }
